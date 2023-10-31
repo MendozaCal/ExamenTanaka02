5167b7538cc4d06f2d4a727d08e2c097d9073d0a
Pablo Mendoza <asdson5555@gmail.com>
2023-10-31 16:03:25 -0500

    Life System

---
+++ Assets/Scrips/Player/HealthSystem.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    [SerializeField]
+    int life;
+    void Update()
+    {
+        if (life <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
