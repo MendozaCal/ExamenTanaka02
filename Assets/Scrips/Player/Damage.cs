1dee701685a5835207d6e2443c8b9ca6ce5a0159
Pablo Mendoza <asdson5555@gmail.com>
2023-10-31 16:00:54 -0500

    Shoot Player

---
+++ Assets/Scrips/Player/Damage.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Damage : MonoBehaviour
+{
+    [SerializeField]
+    int life;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("EnemyUp") || collision.gameObject.CompareTag("Enemyright") || collision.gameObject.CompareTag("BulletEnemy"))
+        {
+            RecibirDaño(collision.gameObject.GetComponent<Damage>().GetDamage());
+        }
+    }
+
+    private void RecibirDaño(int value)
+    {
+        life -= value;
+        if (life <= 0)
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(3);
+        }
+    }
+}
---
+++ Assets/Scrips/Player/PlayerShoot.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject bulletPrefab;
+    [SerializeField]
+    private GameObject bulletPrefab2;
+    private float shootTimer;
+    [SerializeField]
+    private float shootDelayTime;
+    void Update()
+    {
+        Timer();
+        Shoot();
+    }
+    void Timer()
+    {
+        shootTimer += Time.deltaTime;
+    }
+    void Shoot()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && shootTimer >= shootDelayTime)
+        {
+            GameObject obj = Instantiate(bulletPrefab);
+            obj.transform.position = transform.position;
+            shootTimer = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse1) && shootTimer >= shootDelayTime)
+        {
+            GameObject obj = Instantiate(bulletPrefab2);
+            obj.transform.position = transform.position;
+            shootTimer = 0;
+        }
+    }
+}
